772fc7538d93e5c358a5ca361026c9d5539960ab
Mayank Raj <mraj602@gmail.com>
2024-03-28 12:33:42 +0530

    added instructions

---
+++ src/data_extractor/Amazon_scraper.java
@@ -0,0 +1,10 @@
+package data_extractor;
+
+public class Amazon_scraper {
+    // Create multiple functions to capture the data from amazon webpage
+    public static void main(String[] args) {
+        System.out.println("Calling multiple functions here");
+        System.out.println("There should be one fn for extracting data from  product page and another for reviews page.");
+        System.out.println("There should be no main function in this class. The constructor when called with any URL, it should scrape all the data and when get_result() method is called, it returns the data");
+    }
+}
