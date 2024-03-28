eca88a03f935b4828746cd282c94721ea8dc5858
Mayank Raj <mraj602@gmail.com>
2024-03-28 12:41:23 +0530

    Added branch info

--- src/data_extractor/Amazon_scraper.java
+++ src/data_extractor/Amazon_scraper.java
@@ -6,5 +6,6 @@ public class Amazon_scraper {
         System.out.println("Calling multiple functions here");
         System.out.println("There should be one fn for extracting data from  product page and another for reviews page.");
         System.out.println("There should be no main function in this class. The constructor when called with any URL, it should scrape all the data and when get_result() method is called, it returns the data");
+        System.out.println("Created branch 'data_extraction' in which Nandini should work");
     }
 }
